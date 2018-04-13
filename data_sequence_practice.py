18fd0cc102c987a48152d02a862ad2c8fbad4b2f
Kyla Brown <kylabrown1536@gmail.com>
2018-04-13 08:42:02 -0400

    Initial commit

---
+++ data_sequence_practice.py
@@ -0,0 +1,10 @@
+list = ['Chocolate', 'Vanilla', 'Strawberry', 'Chocolate chip', 'Mint']
+print (list[0])
+list.append('Butter Pecan')
+print(list)
+list.remove('Vanilla')
+print(list)
+['Chocolate', 'Strawberry', 'Chocolate chip', 'Mint', 'Butter Pecan']
+tuple(list)
+dict = {'Chocolate': "My favorite ice cream flavor!", 'Strawberry': "It tastes like fruit.", "Chocolate chip": "Also one of the best", 'Mint': "Yuck.", "Butter Pecan": "My dad's favorite."}
+print (dict['Strawberry'])
